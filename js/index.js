668e160ab81d98e6b25d261da9f8c802c978ea18
jalvaradoWD <jose-alvarado@lambdastudents.com>
2020-08-08 13:22:49 -0500

    Added three event listeners for images. - Allows to change colors of the images borders to a random color.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
-// Your code goes here
+const buttons = document.querySelectorAll(".btn");
 const images = document.querySelectorAll("img");
 
-images.forEach(element => {
-    element.addEventListener("drag", () => {
 
+
+buttons.forEach(button => {
+    button.addEventListener("dblclick", e => {
+        e.target.style.background = "black"
     })
+})
+
+const midColors = () => {
+    const randomVal = Math.floor(Math.random() * 255);
+    if (randomVal < 80) {
+        return 80
+    } else if (randomVal > 200) {
+        return 200
+    } else {
+        return randomVal
+    }
+}
+
+const changeBorderColor = e => e.target.style.borderColor = `rgba(${midColors()}, ${midColors()}, ${midColors()}, 1)`
+
+images.forEach(image => {
+    image.addEventListener("click", e => {
+        e.target.style.border = "10px solid rgba(0, 0, 0, 1)"
+    })
+
+    image.addEventListener("mouseenter", e => changeBorderColor(e))
+    image.addEventListener("mouseleave", e => changeBorderColor(e))
 })
