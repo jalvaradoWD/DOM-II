5d32c897277bb0897ef4f28cb21a781412033a90
jalvaradoWD <josealvarado1337@gmail.com>
2020-08-08 09:56:01 -0500

    First init

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Your code goes here
 const images = document.querySelectorAll("img");
 
-images.forEach(element => {
-    element.addEventListener("drag", () => {
+images.forEach((element) => {
+	element.addEventListener("mousedown", (selfElement) => {
+		selfElement.target.style.border = "5px solid black";
+	});
 
-    })
-})
+	element.addEventListener("mouseup", (selfElement) => {
+		selfElement.target.style.border = "none";
+	});
+});
